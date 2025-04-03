daf405b6553cfc5e723ec4450b19b3bbf87cdcf0
22421 <22421@wc.school.nz>
2025-04-04 12:30:39 +1300

    Version 1
    Created the htmls of the 3d printer service and managed to create a directory of switching to htmls and stuff

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_homepage():
+    return render_template('Home.html')
 
+@app.route('/Session')
+def render_Sessionpage():
+    return render_template('Session.html')
+
+@app.route('/images')
+def render_imagespage():
+    return render_template('images.html')
 
 if __name__ == '__main__':
     app.run()
