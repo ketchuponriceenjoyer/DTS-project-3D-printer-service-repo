ae7f4da5f9c5e78429c85ee0980a1b4d4faed7eb
22421 <22421@wc.school.nz>
2025-04-11 12:28:20 +1200

    Version 1.4
    fixed the css and improved it better to be more organsied and understandable

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
 
+Database = ''
 
 @app.route('/')
 def render_homepage():
     return render_template('Home.html')
 
-@app.route('/Session')
+@app.route('/Session', methods=['POST','GET'])
 def render_Sessionpage():
+    if request.method == 'POST':
+        Name = request.form.get('Name')
+        Address = request.form.get('Address')
+        Filament = request.form.get('Filament')
+        Size = request.form.get('Size')
+        Model = request.form.get('Model_File')
     return render_template('Session.html')
 
 @app.route('/images')
